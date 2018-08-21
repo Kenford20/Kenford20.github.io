4cb681bfe5f3c896aac7109c665a62331657529b
Kenford <kzhou0066@gmail.com>
2018-08-21 04:25:47 -0400

    again

--- client/client.js
+++ client/client.js
@@ -49,7 +49,7 @@ var cardType = {
 //var socket = io.connect();
 var host = window.location.origin; 
 console.log(host);
-var socket = io(host);
+var socket = io.connect("https://kenford20.github.io/server.js");
 //var socket = io.connect('http://' + host + ":"  + 3000);
 
 
